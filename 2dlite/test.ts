ae151f14708ef486a5c89fb2b21868a9ff330e70
SvenFrankson <sven.frankson@gmail.com>
2019-08-20 02:10:54 +0200

    Add small test for nesting parenting.

--- 2dlite/test.ts
+++ 2dlite/test.ts
@@ -45,6 +45,34 @@ class SpaceShip extends LineMesh {
     }
 }
 
+class AltSpaceShip extends LineMesh {
+
+    private _k: number = 0;
+
+    public start(): void {
+        let body = new LineMesh("body", V.N(), 0, 1);
+        body.parent = this;
+        body.lines = [Line.Parse("white:-10,-10 -10,10 0,25 10,10 10,-10 -10,-10")];
+        body.instantiate();
+        let wingR = new LineMesh("wingR", V.N(20, 0), 0, 1);
+        wingR.parent = body;
+        wingR.lines = [Line.Parse("red:-4,-5 -4,10 20,0 20,-10 -4,-5")];
+        wingR.instantiate();
+        let wingL = new LineMesh("wingL", V.N(0, -5), 0, 1);
+        wingL.parent = body;
+        wingL.lines = [Line.Parse("red:-16,0 -16,15 -40,5 -40,-5 -16,0")];
+        wingL.instantiate();
+    }
+
+    public update(): void {
+        this.p.x = 100 * Math.cos(this._k / 100);
+        this.p.y = 50 * Math.sin(this._k / 50);
+        this.r = this._k / 60;
+        this.s = 1.5 + Math.sin(this._k / 50);
+        this._k++;
+    }
+}
+
 window.onload = () => {
     let canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
     canvas.width = 400;
@@ -55,7 +83,7 @@ window.onload = () => {
     let camera = new Camera();
     camera.setW(400, canvas);
     camera.instantiate();
-    let mesh = new SpaceShip();
+    let mesh = new AltSpaceShip();
     mesh.instantiate();
     en.start();
     setTimeout(
